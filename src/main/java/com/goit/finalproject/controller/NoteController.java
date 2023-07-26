b2326fef9682ec991846362f8e9388df30b87054
volodymyrshynkarenko <volodymyr.shynkarenko.digital@gmail.com>
2023-07-26 23:56:52 +0300

    upd JpaRepository<Note, Long> {     List<Note> findAllByUserId(Long userId);     UPD HTML

--- src/main/java/com/goit/finalproject/controller/NoteController.java
+++ src/main/java/com/goit/finalproject/controller/NoteController.java
@@ -1,7 +1,8 @@
 package com.goit.finalproject.controller;
 
+import com.goit.finalproject.models.Note;
 import com.goit.finalproject.models.NoteDto;
-import com.goit.finalproject.repository.NoteRepository;
+import com.goit.finalproject.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
@@ -11,30 +12,29 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/note")
 public class NoteController {
-
-    private final NoteRepository noteRepository;
+    //  сервіс замість репозиторію?
+    private final NoteService noteService;
 
     @Autowired
-    public NoteController(NoteRepository noteRepository) {
-        this.noteRepository = noteRepository;
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
     }
 
     @PostMapping("/add")
-    public ModelAndView addNote(NoteDto note) {
-        noteRepository.save(note);
+    public ModelAndView addNote(NoteDto noteDto) {
+        noteService.save(noteDto);
         return new ModelAndView("redirect:/path_to_note_listing_page");
     }
 
     @GetMapping("/share/{id}")
     public ModelAndView shareNote(@PathVariable Long id) {
-
         return new ModelAndView("redirect:/path_to_note_listing_page");
     }
 
     @GetMapping("/delete/{id}")
     public ModelAndView deleteNote(@PathVariable Long id) {
         try {
-            noteRepository.deleteById(id);
+            noteService.deleteById(id);
         } catch (EmptyResultDataAccessException e) {
 
         }
--- src/main/java/com/goit/finalproject/repository/NoteRepository.java
+++ src/main/java/com/goit/finalproject/repository/NoteRepository.java
@@ -1,12 +1,7 @@
 package com.goit.finalproject.repository;
-
-import com.goit.finalproject.models.NoteDto;
+import com.goit.finalproject.models.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-
-public interface NoteRepository extends JpaRepository<NoteDto, Long> {
-
-    List<NoteDto> findAllByUserId(Long userId);
-
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findAllByUserId(Long userId);
 }
