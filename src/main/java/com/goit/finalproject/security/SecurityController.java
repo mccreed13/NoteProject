e865e555573708817dbce2e35a86e83458258488
vadim novitskyi <thrownovitskyi@gmail.com>
2023-08-02 20:24:35 +0300

    fix bug

--- src/main/java/com/goit/finalproject/security/SecurityController.java
+++ src/main/java/com/goit/finalproject/security/SecurityController.java
@@ -5,13 +5,11 @@ import com.goit.finalproject.user.UserRepository;
 import com.goit.finalproject.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
