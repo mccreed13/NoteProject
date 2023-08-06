6c511d7495b8c770f9513f98eb8c5b8d19d1474d
Syvolapenko <isivolap@gmail.com>
2023-08-06 18:49:04 +0300

    fix checkstyle errors

--- src/test/java/com/goit/finalproject/security/SecurityControllerTest.java
+++ src/test/java/com/goit/finalproject/security/SecurityControllerTest.java
@@ -1,28 +1,20 @@
 package com.goit.finalproject.security;
-
-import com.goit.finalproject.note.NoteService;
 import com.goit.finalproject.user.UserRepository;
 import com.goit.finalproject.user.UserService;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @ActiveProfiles("dev")
-//@WebMvcTest(SecurityController.class)
 @SpringBootTest
 class SecurityControllerTest {
     @Autowired
--- src/test/java/com/goit/finalproject/user/UserControllerTest.java
+++ src/test/java/com/goit/finalproject/user/UserControllerTest.java
@@ -10,70 +10,58 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @SpringBootTest
 class UserControllerTest {
     @Autowired
     private UserRepository userRepository;
-
     @Autowired
     private UserService userService;
-
     @Autowired
     private RoleService roleService;
-
     @Autowired
     private WebApplicationContext wac;
-
     private MockMvc mvc;
-
     @BeforeEach
     public void setup() {
         mvc = MockMvcBuilders.webAppContextSetup(wac)
                 .apply(springSecurity())
                 .build();
     }
-
     @Test
     void testExistingUserLoginSuccessful() throws Exception {
         Role userRole = new Role(1L, "USER", new ArrayList<>());
         roleService.save(userRole);
-
         User user = new User();
         user.setUsername("Wladyslaw");
         user.setPassword("password");
         user.setRoles(Set.of(userRole));
         userService.createUser(user);
-
         mvc.perform(formLogin("/login")
                         .user(user.getUsername())
                         .password("password"))
                 .andDo(print())
                 .andExpect(status().is3xxRedirection())
-                .andExpect(header().string("Location","/note/list"));
+                .andExpect(header().string("Location", "/note/list"));
     }
-
     @Test
     void testWrongCredentialLoginFailed() throws Exception {
         Role userRole = new Role(1L, "USER", new ArrayList<>());
         roleService.save(userRole);
-
         User user2 = new User();
         user2.setUsername("Maksym");
         user2.setPassword("password");
         user2.setRoles(Set.of(userRole));
         userService.createUser(user2);
-
         mvc.perform(formLogin("/login")
                         .user(user2.getUsername())
                         .password("wrong_password"))
@@ -81,14 +69,12 @@ class UserControllerTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(header().string("Location", "/login?error"));
     }
-
     @Test
     @WithMockUser(value = "USER")
-    public void whenAdminAccessAdminSecuredEndpoint_thenIsOk() throws Exception {
+    public void whenAdminAccessAdminSecuredEndpointThenIsOk() throws Exception {
         mvc.perform(get("/note/list"))
                 .andExpect(status().isOk());
     }
-
     @Test
     void testNotExistingUserLoginSuccessful() throws Exception {
         User user3 = new User();
@@ -96,16 +82,12 @@ class UserControllerTest {
         user3.setPassword("password");
         Role userRole = new Role(1L, "USER", new ArrayList<>());
         roleService.save(userRole);
-
         user3.setRoles(new HashSet<Role>(Collections.singletonList(userRole)));
         userService.createUser(user3);
-
-
         mvc.perform(formLogin("/login")
                         .user(user3.getUsername())
                         .password(user3.getPassword()))
                 .andDo(print())
                 .andExpect(status().is3xxRedirection());
     }
-
 }
