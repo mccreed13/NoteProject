0d9a3dc404b8aa145023629657e8da29a7419839
vadim novitskyi <thrownovitskyi@gmail.com>
2023-08-07 19:49:30 +0300

    fix admin office

--- src/main/java/com/goit/finalproject/note/NoteController.java
+++ src/main/java/com/goit/finalproject/note/NoteController.java
@@ -7,7 +7,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
