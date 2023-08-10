890fa41239a04246c8ff41e9a843c69d0d0ef99d
Svyatoslav Shypka <svyat200909@gmail.com>
2023-08-10 16:14:34 +0200

    Corrected tests. Now they use database in memory instead of production one.

--- src/test/java/com/goit/finalproject/FinalProjectApplicationTests.java
+++ src/test/java/com/goit/finalproject/FinalProjectApplicationTests.java
@@ -1,13 +1,13 @@
 package com.goit.finalproject;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FinalProjectApplicationTests {
 
-    @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectApplicationTests.class, args);
     }
 
 }
